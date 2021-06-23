f50af594044f4c8037638663cf05ef3ba608b636
sadko4u <sadko4u@gmail.com>
2021-06-24 01:06:42 +0300

    Added dependency on the lsp-plugins-spectrum-analyzer plugin module

--- plugins.mk
+++ plugins.mk
@@ -21,19 +21,25 @@
 # Specify plugin package version
 PLUGIN_PACKAGE_VERSION                  := 0.0.1
 
-# Specify description of plugin dependency
+# Specify description of plugin dependencies
 LSP_PLUGINS_COMP_DELAY_VERSION          := 1.0.3
 LSP_PLUGINS_COMP_DELAY_NAME             := lsp-plugins-comp-delay
 LSP_PLUGINS_COMP_DELAY_TYPE             := plug
 LSP_PLUGINS_COMP_DELAY_URL              := git@github.com:sadko4u/$(LSP_PLUGINS_COMP_DELAY_NAME).git
 
-# Specify description of plugin dependency
 LSP_PLUGINS_PHASE_DETECTOR_VERSION      := 1.0.0
 LSP_PLUGINS_PHASE_DETECTOR_NAME         := lsp-plugins-phase-detector
 LSP_PLUGINS_PHASE_DETECTOR_TYPE         := plug
 LSP_PLUGINS_PHASE_DETECTOR_URL          := git@github.com:sadko4u/$(LSP_PLUGINS_PHASE_DETECTOR_NAME).git
 
+LSP_PLUGINS_SPECTRUM_ANALYZER_VERSION   := 1.0.0
+LSP_PLUGINS_SPECTRUM_ANALYZER_NAME      := lsp-plugins-spectrum-analyzer
+LSP_PLUGINS_SPECTRUM_ANALYZER_TYPE      := plug
+LSP_PLUGINS_SPECTRUM_ANALYZER_URL       := git@github.com:sadko4u/$(LSP_PLUGINS_SPECTRUM_ANALYZER_NAME).git
+
 # List of all plugin dependencies
 PLUGIN_DEPENDENCIES = \
   LSP_PLUGINS_COMP_DELAY \
-  LSP_PLUGINS_PHASE_DETECTOR
+  LSP_PLUGINS_PHASE_DETECTOR \
+  LSP_PLUGINS_SPECTRUM_ANALYZER
+
