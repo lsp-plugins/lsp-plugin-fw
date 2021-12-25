5ce4bc656eec9018c755186b2895fd46e20f2382
sadko4u <sadko4u@gmail.com>
2021-12-25 21:44:41 +0300

    Refactoring of jack_make utility

--- include/lsp-plug.in/plug-fw/util/common/checksum.h
+++ include/lsp-plug.in/plug-fw/util/common/checksum.h
@@ -25,8 +25,8 @@
 #include <lsp-plug.in/plug-fw/version.h>
 
 #include <lsp-plug.in/common/types.h>
+#include <lsp-plug.in/common/status.h>
 #include <lsp-plug.in/io/Path.h>
-#include <lsp-plug.in/stdlib/stdio.h>
 
 namespace lsp
 {
@@ -39,44 +39,22 @@ namespace lsp
             uint64_t ck3;
         } checksum_t;
 
-        static bool match_checksum(const checksum_t *c1, const checksum_t *c2)
-        {
-            return (c1->ck1 == c2->ck1) &&
-                   (c1->ck2 == c2->ck2) &&
-                   (c1->ck3 == c2->ck3);
-        }
+        /**
+         * Ensure that one checksum matches another checksum
+         * @param c1 checksum 1
+         * @param c2 checksum 2
+         * @return true if checsums match
+         */
+        bool match_checksum(const checksum_t *c1, const checksum_t *c2);
 
-        static status_t calc_checksum(checksum_t *dst, const io::Path *file)
-        {
-            uint8_t buf[0x400];
+        /**
+         * Compute checksum of the file
+         * @param dst destination checksum to store data
+         * @param file path to the file
+         * @return status of operation
+         */
+        status_t calc_checksum(checksum_t *dst, const io::Path *file);
 
-            dst->ck1   = 0;
-            dst->ck2   = 0;
-            dst->ck3   = 0xffffffff;
-
-            FILE *in = fopen(file->as_native(), "r");
-            if (in == NULL)
-                return STATUS_OK;
-
-            while (true)
-            {
-                int nread = fread(buf, sizeof(uint8_t), sizeof(buf), in);
-                if (nread <= 0)
-                    break;
-
-                for (int i=0; i<nread; ++i)
-                {
-                    // Compute checksums
-                    dst->ck1 = dst->ck1 + buf[i];
-                    dst->ck2 = ((dst->ck2 << 9) | (dst->ck2 >> 55)) ^ buf[i];
-                    dst->ck3 = ((dst->ck3 << 17) | (dst->ck3 >> 47)) ^ uint16_t(0xa5ff - buf[i]);
-                }
-            }
-
-            fclose(in);
-
-            return STATUS_OK;
-        }
     } /* namespace util */
 } /* namespace lsp */
 
--- src/util/jack_make/jack_make.cpp
+++ src/util/jack_make/jack_make.cpp
@@ -19,11 +19,9 @@
  * along with lsp-plugin-fw. If not, see <https://www.gnu.org/licenses/>.
  */
 
-#include <lsp-plug.in/common/types.h>
 #include <lsp-plug.in/plug-fw/plug.h>
+#include <lsp-plug.in/plug-fw/util/jack_make/jack_make.h>
 #include <lsp-plug.in/plug-fw/util/common/checksum.h>
-#include <lsp-plug.in/io/Path.h>
-#include <lsp-plug.in/io/Dir.h>
 
 #include <stdio.h>
 #include <string.h>
@@ -32,7 +30,7 @@
 
 namespace lsp
 {
-    namespace vst2_make
+    namespace jack_make
     {
         static ssize_t meta_sort_func(const meta::plugin_t *a, const meta::plugin_t *b)
         {
@@ -66,9 +64,10 @@ namespace lsp
             return STATUS_OK;
         }
 
-        static status_t gen_cpp_file(const io::Path *file, const LSPString *name, const meta::plugin_t *meta)
+        status_t gen_cpp_file(const io::Path *file, const meta::plugin_t *meta)
         {
             // Generate file
+            LSPString fname;
             FILE *out = fopen(file->as_native(), "w");
             if (out == NULL)
             {
@@ -77,11 +76,15 @@ namespace lsp
                 return STATUS_IO_ERROR;
             }
 
+            status_t res = file->get_last(&fname);
+            if (res != STATUS_OK)
+                return res;
+
             // Write to file
             fprintf(out,    "//------------------------------------------------------------------------------\n");
-            fprintf(out,    "// File:            %s\n", name->get_utf8());
-            fprintf(out,    "// VST2 Plugin:     %s - %s [VST2]\n", meta->name, meta->description);
-            fprintf(out,    "// VST2 UID:        '%s'\n", meta->vst2_uid);
+            fprintf(out,    "// File:            %s\n", fname.get_utf8());
+            fprintf(out,    "// JACK Plugin:     %s - %s [JACK]\n", meta->name, meta->description);
+            fprintf(out,    "// JACK UID:        '%s'\n", meta->uid);
             fprintf(out,    "// Version:         %d.%d.%d\n",
                     LSP_MODULE_VERSION_MAJOR(meta->version),
                     LSP_MODULE_VERSION_MINOR(meta->version),
@@ -91,16 +94,16 @@ namespace lsp
 
             // Write code
             fprintf(out,    "// Pass Plugin UID for factory function\n");
-            fprintf(out,    "#define VST2_PLUGIN_UID     \"%s\"\n", meta->vst2_uid);
+            fprintf(out,    "#define JACK_PLUGIN_UID     \"%s\"\n", meta->uid);
             fprintf(out,    "\n");
 
             fprintf(out,    "#include <lsp-plug.in/common/types.h>\n");
             fprintf(out,    "\n");
 
             fprintf(out,    "// Include factory function implementation\n");
-            fprintf(out,    "#define LSP_PLUG_IN_VST2_MAIN_IMPL\n");
-            fprintf(out,    "    #include <lsp-plug.in/plug-fw/wrap/vst2/main.h>\n");
-            fprintf(out,    "#undef LSP_PLUG_IN_VST2_MAIN_IMPL\n");
+            fprintf(out,    "#define LSP_PLUG_IN_JACK_MAIN_IMPL\n");
+            fprintf(out,    "    #include <lsp-plug.in/plug-fw/wrap/jack/main.h>\n");
+            fprintf(out,    "#undef LSP_PLUG_IN_JACK_MAIN_IMPL\n");
 
             // Close file
             fclose(out);
@@ -108,9 +111,10 @@ namespace lsp
             return STATUS_OK;
         }
 
-        static status_t gen_source_file(const io::Path *file, const io::Path *temp, const meta::plugin_t *meta)
+        status_t gen_source_file(const io::Path *file, const meta::plugin_t *meta)
         {
             LSPString fname;
+            io::Path temp;
             status_t res;
 
             // Get file name
@@ -120,22 +124,28 @@ namespace lsp
             // Generate temporary file
             if (file->exists())
             {
+                // Generate temporary file name
+                if ((res = temp.set(file)) != STATUS_OK)
+                    return res;
+                if ((res = temp.append(".tmp")) != STATUS_OK)
+                    return res;
+
                 // Generate temporary file
-                if ((res = gen_cpp_file(temp, &fname, meta)) != STATUS_OK)
+                if ((res = gen_cpp_file(&temp, meta)) != STATUS_OK)
                     return res;
 
                 // Compute checksums of file
                 util::checksum_t f1, f2;
-                if ((res = util::calc_checksum(&f1, temp)) != STATUS_OK)
+                if ((res = util::calc_checksum(&f1, &temp)) != STATUS_OK)
                     return res;
                 if ((res = util::calc_checksum(&f2, file)) != STATUS_OK)
                     return res;
 
                 if (!util::match_checksum(&f1, &f2))
                 {
-                    if ((res = temp->rename(file)) != STATUS_OK)
+                    if ((res = temp.rename(file)) != STATUS_OK)
                     {
-                        fprintf(stderr, "Could not rename file '%s' to '%s'\n", temp->as_native(), file->as_native());
+                        fprintf(stderr, "Could not rename file '%s' to '%s'\n", temp.as_native(), file->as_native());
                         return res;
                     }
 
@@ -144,7 +154,7 @@ namespace lsp
                 }
                 else
                 {
-                    if ((res = temp->remove()) != STATUS_OK)
+                    if ((res = temp.remove()) != STATUS_OK)
                     {
                         fprintf(stderr, "Could not remove file: '%s'\n", file->as_native());
                         return res;
@@ -154,7 +164,7 @@ namespace lsp
             else
             {
                 // Generate direct file
-                if ((res = gen_cpp_file(file, &fname, meta)) != STATUS_OK)
+                if ((res = gen_cpp_file(file, meta)) != STATUS_OK)
                     return res;
 
                 // Output information
@@ -164,9 +174,9 @@ namespace lsp
             return STATUS_OK;
         }
 
-        status_t make_filename(LSPString *fname, const char *lv2_uid)
+        status_t make_filename(LSPString *fname, const meta::plugin_t *meta)
         {
-            if (!fname->set_ascii(lv2_uid))
+            if (!fname->set_ascii(meta->uid))
                 return STATUS_NO_MEM;
             fname->replace_all('_', '-');
             return STATUS_OK;
@@ -211,7 +221,7 @@ namespace lsp
             {
                 // Get plugin metadata
                 const meta::plugin_t *meta = list->uget(i);
-                if ((res = make_filename(&fname, meta->uid)) != STATUS_OK)
+                if ((res = make_filename(&fname, meta)) != STATUS_OK)
                     return res;
 
                 fprintf(out, "  %s.cpp", fname.get_utf8());
@@ -222,26 +232,26 @@ namespace lsp
             }
 
             fprintf(out, "# Output files\n");
-            fprintf(out, "OBJ_FILES = \\\n");
+            fprintf(out, "EXE_FILES = \\\n");
             for (size_t i=0, n=list->size(); i<n; )
             {
                 // Get plugin metadata
                 const meta::plugin_t *meta = list->uget(i);
-                if ((res = make_filename(&fname, meta->uid)) != STATUS_OK)
+                if ((res = make_filename(&fname, meta)) != STATUS_OK)
                     return res;
 
-                fprintf(out, "  %s$(LIBRARY_EXT)", fname.get_utf8());
+                fprintf(out, "  $(EXT_PREFIX)%s$(EXECUTABLE_EXT)", fname.get_utf8());
                 if (++i >= n)
                     fprintf(out, "\n\n");
                 else
                     fprintf(out, " \\\n");
             }
 
-            fprintf(out, "FILE = $(@:%%$(LIBRARY_EXT)=%%.cpp)\n");
+            fprintf(out, "FILE = $(@:$(EXT_PREFIX)%%$(EXECUTABLE_EXT)=%%.cpp)\n");
             fprintf(out, "DEP_CXX = $(foreach src,$(CXX_FILES),$(patsubst %%.cpp,%%.d,$(src)))\n");
             fprintf(out, "DEP_CXX_FILE = $(patsubst %%.d,%%.cpp,$(@))\n");
-            fprintf(out, "DEP_DEP_FILE = $(patsubst %%.d,%%$(LIBRARY_EXT),$(@))\n");
-            fprintf(out, "VST2_CXX_DEFS = -DEXT_ARTIFACT_ID=\\\"$(EXT_ARTIFACT_ID)\\\"\n");
+            fprintf(out, "DEP_DEP_FILE = $(patsubst %%.d,$(EXT_PREFIX)%%$(EXECUTABLE_EXT),$(@))\n");
+            fprintf(out, "JACK_CXX_DEFS = -DEXT_ARTIFACT_ID=\\\"$(EXT_ARTIFACT_ID)\\\"\n");
             fprintf(out, "\n");
 
             fprintf(out, ".DEFAULT_GOAL := all\n");
@@ -249,23 +259,27 @@ namespace lsp
 
             fprintf(out, "\n");
             fprintf(out, "$(DEP_CXX): dep_clean\n");
-            fprintf(out, "\t$(CXX) -MM -MT \"$(DEP_DEP_FILE)\" -MF $(@) $(CXXFLAGS) $(EXT_CXXFLAGS) $(VST2_CXX_DEFS) $(INCLUDE) $(EXT_INCLUDE) $(DEP_CXX_FILE)\n");
+            fprintf(out, "\t$(CXX) -MM -MT \"$(DEP_DEP_FILE)\" -MF $(@) $(CXXFLAGS) $(EXT_CXXFLAGS) $(JACK_CXX_DEFS) $(INCLUDE) $(EXT_INCLUDE) $(DEP_CXX_FILE)\n");
 
             fprintf(out, "\n");
             fprintf(out, "depend: $(DEP_CXX)\n");
             fprintf(out, "\tcat $(DEP_CXX) >Makefile.d\n");
 
             fprintf(out, "\n");
-            fprintf(out, "all: $(OBJ_FILES)\n");
+            fprintf(out, "all: $(EXE_FILES)\n");
 
             fprintf(out, "\n");
-            fprintf(out, "$(OBJ_FILES):\n");
+            fprintf(out, "$(EXE_FILES):\n");
             fprintf(out, "\techo \"  $(CXX) $(FILE)\"\n");
-            fprintf(out, "\t$(CXX) -o $(@) $(CXXFLAGS) $(EXT_CXXFLAGS) $(VST2_CXX_DEFS) $(INCLUDE) $(EXT_INCLUDE) $(FILE) $(EXT_OBJS) $(LIBS) $(SO_FLAGS) $(EXT_LDFLAGS)\n");
+            fprintf(out, "\t$(CXX) -o $(@) $(CXXFLAGS) $(EXT_CXXFLAGS) $(JACK_CXX_DEFS) $(INCLUDE) $(EXT_INCLUDE) $(FILE) $(EXT_OBJS) $(LIBS) $(EXE_FLAGS) $(EXT_LDFLAGS)\n");
 
             fprintf(out, "\n");
-            fprintf(out, "install: $(OBJ_FILES)\n");
-            fprintf(out, "\t$(INSTALL) $(OBJ_FILES) $(DESTDIR)/\n");
+            fprintf(out, "install: $(EXE_FILES)\n");
+            fprintf(out, "\t$(INSTALL) $(EXE_FILES) $(DESTDIR)/\n");
+
+            fprintf(out, "\n");
+            fprintf(out, "uninstall: \n");
+            fprintf(out, "\t-rm -f $(addprefix $(DESTDIR)/,$(EXE_FILES))\n");
 
             fprintf(out, "\n");
             fprintf(out, "# Dependencies\n");
@@ -279,7 +293,7 @@ namespace lsp
 
         status_t main(const char *base)
         {
-            io::Path path, file, temp;
+            io::Path path, file;
             LSPString fname;
             lltl::parray<meta::plugin_t> list;
 
@@ -301,39 +315,25 @@ namespace lsp
                 const meta::plugin_t *meta = list.uget(i);
 
                 // Get file path
-                if ((res = make_filename(&fname, meta->uid)) != STATUS_OK)
+                if ((res = make_filename(&fname, meta)) != STATUS_OK)
                     return res;
                 if (!fname.append_ascii(".cpp"))
                     return STATUS_NO_MEM;
                 if ((res = file.set(&path, &fname)) != STATUS_OK)
                     return res;
-                if (!fname.append_ascii(".tmp"))
-                    return STATUS_NO_MEM;
-                if ((res = temp.set(&path, &fname)) != STATUS_OK)
-                    return res;
 
                 // Generate C++ file
-                if ((res = gen_source_file(&file, &temp, meta)) != STATUS_OK)
+                if ((res = gen_source_file(&file, meta)) != STATUS_OK)
+                {
+                    fprintf(stderr, "Error generating source file %s: error code %d", file.as_native(), res);
                     return res;
+                }
             }
 
             return gen_makefile(&path, &list);
         }
-    } /* vst2_make */
-} /* lsp */
-
-#ifndef LSP_IDE_DEBUG
-int main(int argc, const char **argv)
-{
-    if (argc <= 0)
-        fprintf(stderr, "required destination path");
-    lsp::status_t res = lsp::vst2_make::main(argv[1]);
-    if (res != lsp::STATUS_OK)
-        fprintf(stderr, "Error while generating build files, code=%d", int(res));
-
-    return res;
+    }
 }
-#endif /* LSP_IDE_DEBUG */
 
 
 
