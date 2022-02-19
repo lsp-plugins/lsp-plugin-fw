e494c59228a16eb6452ac2e9691128c33b5df052
sadko4u <sadko4u@gmail.com>
2022-02-19 22:36:08 +0300

    Updated PHP generation tool.

--- src/util/pluglist_gen/php.cpp
+++ src/util/pluglist_gen/php.cpp
@@ -58,6 +58,7 @@ namespace lsp
 
                     case '\'':
                     case '\"':
+                    case '$':
                     case '\\':
                         buf.append('\\');
                         break;
@@ -92,7 +93,7 @@ namespace lsp
                 {
                     if ((items++) > 0)
                         fprintf(out, ", ");
-                    fprintf(out, "'%s'", grp);
+                    fprintf(out, "\"%s\"", grp);
                 }
             }
 
@@ -109,21 +110,21 @@ namespace lsp
                 int(LSP_MODULE_VERSION_MICRO(m->version))
             );
 
-            fprintf(out, "\t\t\t'id' => '%s',\n", php_escape(buf, m->uid));
-            fprintf(out, "\t\t\t'name' => '%s',\n", php_escape(buf, m->name));
-            fprintf(out, "\t\t\t'author' => '%s',\n", php_escape(buf, m->developer->name));
-            fprintf(out, "\t\t\t'version' => '%s',\n", php_escape(buf, &tmp));
+            fprintf(out, "\t\t\t'id' => \"%s\",\n", php_escape(buf, m->uid));
+            fprintf(out, "\t\t\t'name' => \"%s\",\n", php_escape(buf, m->name));
+            fprintf(out, "\t\t\t'author' => \"%s\",\n", php_escape(buf, m->developer->name));
+            fprintf(out, "\t\t\t'version' => \"%s\",\n", php_escape(buf, &tmp));
             if ((m->bundle) && (m->bundle->uid))
-                fprintf(out, "\t\t\t'bundle' => '%s',\n", php_escape(buf, m->bundle->uid));
+                fprintf(out, "\t\t\t'bundle' => \"%s\",\n", php_escape(buf, m->bundle->uid));
             else
                 fprintf(out, "\t\t\t'bundle' => null,\n");
-            fprintf(out, "\t\t\t'description' => '%s',\n", php_escape(buf, m->description));
-            fprintf(out, "\t\t\t'acronym' => '%s',\n", php_escape(buf, m->acronym));
+            fprintf(out, "\t\t\t'description' => \"%s\",\n", php_escape(buf, m->description));
+            fprintf(out, "\t\t\t'acronym' => \"%s\",\n", php_escape(buf, m->acronym));
 
             if (m->ladspa_id > 0)
             {
                 fprintf(out, "\t\t\t'ladspa_uid' => '%ld',\n", long(m->ladspa_id));
-                fprintf(out, "\t\t\t'ladspa_label' => '%s',\n", php_escape(buf, m->ladspa_lbl));
+                fprintf(out, "\t\t\t'ladspa_label' => \"%s\",\n", php_escape(buf, m->ladspa_lbl));
             }
             else
             {
@@ -131,9 +132,9 @@ namespace lsp
                 fprintf(out, "\t\t\t'ladspa_label' => null,\n");
             }
 
-            fprintf(out, "\t\t\t'lv2_uri' => '%s',\n", php_escape(buf, m->lv2_uri));
-            fprintf(out, "\t\t\t'lv2ui_uri' => '%s',\n", php_escape(buf, m->lv2ui_uri));
-            fprintf(out, "\t\t\t'vst2_uid' => '%s',\n", php_escape(buf, m->vst2_uid));
+            fprintf(out, "\t\t\t'lv2_uri' => \"%s\",\n", php_escape(buf, m->lv2_uri));
+            fprintf(out, "\t\t\t'lv2ui_uri' => \"%s\",\n", php_escape(buf, m->lv2ui_uri));
+            fprintf(out, "\t\t\t'vst2_uid' => \"%s\",\n", php_escape(buf, m->vst2_uid));
             fprintf(out, "\t\t\t'jack' => true,\n");
             fprintf(out, "\t\t\t'groups' => ");
             php_print_plugin_groups(out, m->classes);
@@ -143,11 +144,11 @@ namespace lsp
         static void php_write_bundle(FILE *out, const meta::bundle_t *bundle)
         {
             LSPString buf;
-            fprintf(out, "\t\t\t'id' => '%s',\n", php_escape(buf, bundle->uid));
-            fprintf(out, "\t\t\t'name' => '%s',\n", php_escape(buf, bundle->name));
-            fprintf(out, "\t\t\t'group' => '%s',\n", get_enumeration(bundle->group, bundle_groups));
-            fprintf(out, "\t\t\t'video' => '%s',\n", php_escape(buf, bundle->video_id));
-            fprintf(out, "\t\t\t'description' => '%s'\n", php_escape(buf, bundle->description));
+            fprintf(out, "\t\t\t'id' => \"%s\",\n", php_escape(buf, bundle->uid));
+            fprintf(out, "\t\t\t'name' => \"%s\",\n", php_escape(buf, bundle->name));
+            fprintf(out, "\t\t\t'group' => \"%s\",\n", get_enumeration(bundle->group, bundle_groups));
+            fprintf(out, "\t\t\t'video' => \"%s\",\n", php_escape(buf, bundle->video_id));
+            fprintf(out, "\t\t\t'description' => \"%s\"\n", php_escape(buf, bundle->description));
         }
 
         static void php_write_package_info(FILE *out, const meta::package_t *package)
@@ -162,14 +163,14 @@ namespace lsp
             if (package->version.branch)
                 tmp.fmt_append_utf8("-%s", package->version.branch);
 
-            fprintf(out, "\t\t'artifact' => '%s',\n", php_escape(buf, package->artifact));
-            fprintf(out, "\t\t'name' => '%s',\n", php_escape(buf, package->artifact_name));
-            fprintf(out, "\t\t'version' => '%s',\n", php_escape(buf, &tmp));
-            fprintf(out, "\t\t'brand' => '%s',\n", php_escape(buf, package->brand));
-            fprintf(out, "\t\t'short' => '%s',\n", php_escape(buf, package->short_name));
-            fprintf(out, "\t\t'full' => '%s',\n", php_escape(buf, package->full_name));
-            fprintf(out, "\t\t'license' => '%s',\n", php_escape(buf, package->license));
-            fprintf(out, "\t\t'copyright' => '%s'\n", php_escape(buf, package->copyright));
+            fprintf(out, "\t\t'artifact' => \"%s\",\n", php_escape(buf, package->artifact));
+            fprintf(out, "\t\t'name' => \"%s\",\n", php_escape(buf, package->artifact_name));
+            fprintf(out, "\t\t'version' => \"%s\",\n", php_escape(buf, &tmp));
+            fprintf(out, "\t\t'brand' => \"%s\",\n", php_escape(buf, package->brand));
+            fprintf(out, "\t\t'short' => \"%s\",\n", php_escape(buf, package->short_name));
+            fprintf(out, "\t\t'full' => \"%s\",\n", php_escape(buf, package->full_name));
+            fprintf(out, "\t\t'license' => \"%s\",\n", php_escape(buf, package->license));
+            fprintf(out, "\t\t'copyright' => \"%s\"\n", php_escape(buf, package->copyright));
         }
 
         status_t generate_php(
