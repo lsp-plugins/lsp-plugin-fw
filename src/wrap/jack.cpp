70d8f9193e2cbd66e287eea6b47cdffdcbe6e073
Vladimir Sadovnikov <sadko4u@gmail.com>
2022-12-24 21:14:53 +0300

    Updated header for JACK

--- src/wrap/jack.cpp
+++ src/wrap/jack.cpp
@@ -2,21 +2,21 @@
  * Copyright (C) 2020 Linux Studio Plugins Project <https://lsp-plug.in/>
  *           (C) 2020 Vladimir Sadovnikov <sadko4u@gmail.com>
  *
- * This file is part of lsp-plugins
+ * This file is part of lsp-plugin-fw
  * Created on: 11 мая 2016 г.
  *
- * lsp-plugins is free software: you can redistribute it and/or modify
+ * lsp-plugin-fw is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * any later version.
  *
- * lsp-plugins is distributed in the hope that it will be useful,
+ * lsp-plugin-fw is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public License
- * along with lsp-plugins. If not, see <https://www.gnu.org/licenses/>.
+ * along with lsp-plugin-fw. If not, see <https://www.gnu.org/licenses/>.
  */
 
 #include <jack/jack.h>
