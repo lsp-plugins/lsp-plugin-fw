4a54276ec73f121b534f0664cd83688ceec0ea8a
Vladimir Sadovnikov <sadko4u@gmail.com>
2025-06-22 22:04:18 +0300

    Added tab widget controller

--- include/lsp-plug.in/plug-fw/ctl.h
+++ include/lsp-plug.in/plug-fw/ctl.h
@@ -79,6 +79,7 @@
     #include <lsp-plug.in/plug-fw/ctl/containers/Cell.h>
     #include <lsp-plug.in/plug-fw/ctl/containers/MultiLabel.h>
     #include <lsp-plug.in/plug-fw/ctl/containers/Overlay.h>
+    #include <lsp-plug.in/plug-fw/ctl/containers/Tab.h>
     #include <lsp-plug.in/plug-fw/ctl/containers/TabControl.h>
     #include <lsp-plug.in/plug-fw/ctl/containers/TabGroup.h>
     #include <lsp-plug.in/plug-fw/ctl/containers/ListBox.h>
