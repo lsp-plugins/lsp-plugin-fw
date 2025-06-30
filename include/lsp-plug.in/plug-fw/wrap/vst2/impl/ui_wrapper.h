5ec97cf32600247c8e2a298f3a7bad348f811bfb
Vladimir Sadovnikov <sadko4u@gmail.com>
2025-07-01 01:20:57 +0300

    VST2: notify about preset changed

--- include/lsp-plug.in/plug-fw/wrap/vst2/impl/ui_wrapper.h
+++ include/lsp-plug.in/plug-fw/wrap/vst2/impl/ui_wrapper.h
@@ -1,6 +1,6 @@
 /*
- * Copyright (C) 2024 Linux Studio Plugins Project <https://lsp-plug.in/>
- *           (C) 2024 Vladimir Sadovnikov <sadko4u@gmail.com>
+ * Copyright (C) 2025 Linux Studio Plugins Project <https://lsp-plug.in/>
+ *           (C) 2025 Vladimir Sadovnikov <sadko4u@gmail.com>
  *
  * This file is part of lsp-plugin-fw
  * Created on: 12 дек. 2021 г.
@@ -102,40 +102,44 @@ namespace lsp
 
                 case meta::R_PATH:
                     lsp_trace("creating path port %s", port->id);
-                    vup = new vst2::UIPathPort(port, vp);
+                    vup = new vst2::UIPathPort(port, vp, this);
                     break;
 
                 case meta::R_STRING:
                     lsp_trace("creating string port %s", port->id);
-                    vup = new vst2::UIStringPort(port, vp);
+                    vup = new vst2::UIStringPort(port, vp, this);
                     break;
 
                 case meta::R_SEND_NAME:
                     lsp_trace("creating send name port %s", port->id);
-                    vup = new vst2::UIStringPort(port, vp);
+                    vup = new vst2::UIStringPort(port, vp, this);
                     break;
 
                 case meta::R_RETURN_NAME:
                     lsp_trace("creating return name port %s", port->id);
-                    vup = new vst2::UIStringPort(port, vp);
+                    vup = new vst2::UIStringPort(port, vp, this);
                     break;
 
                 case meta::R_CONTROL:
-                case meta::R_METER:
+                    lsp_trace("creating control port %s", port->id);
+                    vup     = new vst2::UIParameterPort(port, static_cast<vst2::ParameterPort *>(vp), this);
+                    break;
+
                 case meta::R_BYPASS:
-                    lsp_trace("creating regular port %s", port->id);
-                    // VST specifies only INPUT parameters, output should be read in different way
-                    if (meta::is_out_port(port))
-                        vup     = new vst2::UIMeterPort(port, vp);
-                    else
-                        vup     = new vst2::UIParameterPort(port, static_cast<vst2::ParameterPort *>(vp));
+                    lsp_trace("creating bypass port %s", port->id);
+                    vup     = new vst2::UIParameterPort(port, static_cast<vst2::ParameterPort *>(vp), NULL);
+                    break;
+
+                case meta::R_METER:
+                    lsp_trace("creating meter port %s", port->id);
+                    vup     = new vst2::UIMeterPort(port, vp);
                     break;
 
                 case meta::R_PORT_SET:
                 {
                     char postfix_buf[MAX_PARAM_ID_BYTES], param_name[MAX_PARAM_ID_BYTES];
                     lsp_trace("creating port group %s", port->id);
-                    UIPortGroup *upg = new vst2::UIPortGroup(static_cast<vst2::PortGroup *>(vp));
+                    UIPortGroup *upg = new vst2::UIPortGroup(static_cast<vst2::PortGroup *>(vp), this);
 
                     // Add immediately port group to list
                     vPorts.add(upg);
