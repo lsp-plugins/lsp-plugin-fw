7c90a441296b94274ed85fa0bd7e0f395ad075fd
Vladimir Sadovnikov <sadko4u@gmail.com>
2025-07-06 00:51:48 +0300

    Returned grabbing back

--- src/main/ctl/PresetsWindow.cpp
+++ src/main/ctl/PresetsWindow.cpp
@@ -416,7 +416,7 @@ namespace lsp
             }
 
             wnd->show();
-//            wnd->grab_events(ws::GRAB_DROPDOWN);
+            wnd->grab_events(ws::GRAB_DROPDOWN);
 
             return STATUS_OK;
         }
